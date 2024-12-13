5f49684be5ea0a9733e5df0040b487a9c9ab7139
Skye Harris-Stoertz <sharrisstoertz@lcs.on.ca>
2024-12-13 14:24:03 -0500

    Initial Commit

---
+++ DefiningCustomShapes/DefiningCustomShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DefiningCustomShapesApp.swift
+//  DefiningCustomShapes
+//
+//  Created by Skye Willow Harris-Stoertz on 2024-12-13.
+//
+
+import SwiftUI
+
+@main
+struct DefiningCustomShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
